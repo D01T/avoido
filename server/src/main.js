6ba9b1b16e059651f8ca35ab0ca0ce8f5ae5941d
Su-Yong <simssy2205@gmail.com>
2017-08-07 11:40:45 +0900

    add koa framework

--- server/src/main.js
+++ server/src/main.js
@@ -1,1 +1,15 @@
-// for koa
+const koa = require('koa')
+const Pug = require('koa-pug')
+
+const app = new koa()
+const pug = new Pug({
+  viewPath: '../res'
+})
+
+pug.use(app)
+
+app.use(async ctx => {
+  ctx.body = pug.render('../res/index.pug')
+})
+
+app.listen(4000)
