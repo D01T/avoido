c0da34fd5459bf703952e3e01f9d1d934c593cf6
Su-Yong <simssy2205@gmail.com>
2017-08-08 01:31:56 +0900

    터져라!

---
+++ client/public/js/main.js
@@ -0,0 +1,5 @@
+var socket = io()
+
+socket.on('run', function (data) {
+
+})
--- server/src/main.js
+++ server/src/main.js
@@ -1,15 +1,10 @@
 const koa = require('koa');
-const Pug = require('koa-pug');
+const pug = require('pug');
 
 const app = new koa();
-const pug = new Pug({
-  viewPath: '../res',
-});
-
-pug.use(app);
 
 app.use(async (ctx) => {
-  ctx.body = pug.render('../res/index.pug');
+  ctx.body = pug.renderFile('../../client/view/index.pug');
 });
 
 app.listen(4000);
